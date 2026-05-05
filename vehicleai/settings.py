bfe259b1ca7a6a58e8ec969eab102f497c5c01f8
Dhananjayan M <mdhananjayan67@gmail.com>
2026-05-05 12:37:16 +0530

    Modified views, settings

--- vehicleai/settings.py
+++ vehicleai/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_7@iyk5@cs$b8*gos5b1%4zq+55pr6^xq1%kjcaa(wwr7axw^$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
